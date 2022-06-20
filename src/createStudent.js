f04ab768b4c431ef06640d1b2757c06d26de4216
Reinaldo <reinaldoper83@gmail.com>
2022-06-20 18:41:48 -0300

    requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,16 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  let obj = {
+    name: `${nome}`,
+    feedback() {
+      return 'Eita pessoa boa!';
+    },
+  };
+
+  return obj;
+};
+console.log(createStudent('Leandrão, o Lobo Solitário'));
 
 module.exports = createStudent;
