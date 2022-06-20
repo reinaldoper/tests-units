518afcf966875a9f78158ca1b97b934d53616aaa
Reinaldo <reinaldoper83@gmail.com>
2022-06-20 17:55:41 -0300

    comentário

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -38,5 +38,6 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     // Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à circunferência correta para um círculo de raio 2.
     // Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à área correta para um círculo de raio 3.
     // Teste se a função retorna, em um objeto, os dados corretos de um círculo de raio 3.
+    //alterando o push
   });
 });
