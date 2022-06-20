af7b49bd3d8ae54ebd414459aa5631183336efb1
Reinaldo <reinaldoper83@gmail.com>
2022-06-20 16:33:20 -0300

    requisito 1

--- src/average.js
+++ src/average.js
@@ -11,6 +11,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  if (array.length === 0) {
+    return undefined;
+  }
+  let sum = 0;
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] !== 'number') {
+      return undefined;
+    }
+    sum += array[i];
+}
+  return Math.round(sum / array.length);
+};
 
 module.exports = average;
