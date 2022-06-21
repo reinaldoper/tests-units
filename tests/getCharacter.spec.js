35cf701f4d9492b248dfd27ceee8c79e093af6a7
Reinaldo <reinaldoper83@gmail.com>
2022-06-21 13:36:29 -0300

    requisito 9

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -43,7 +43,7 @@ Retorno:
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    fail('Teste vazio!');
+    expect(getCharacter()).toEqual(undefined);
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
@@ -51,5 +51,36 @@ describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
     // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
     // Teste se o parâmetro não é Case Sensitive, ou seja, independente de conter letras maiúsculas ou minúsculas retorna o mesmo objeto relativo a ele.
     // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+  });
+  it('Teste se a função retorna o objeto correto para o parâmetro Arya', () => {
+    expect({
+      name: 'Arya Stark',
+      class: 'Rogue',
+      phrases: [ 'Not today', 'A girl has no name.' ]
+    }).toEqual(getCharacter('Arya'));
+  });
+  it('Teste se a função retorna o objeto correto para o parâmetro Brienne', () => {
+    expect({
+      name: 'Brienne Tarth',
+      class: 'Knight',
+      phrases: [ 'Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.' ]
+    }).toEqual(getCharacter('Brienne'));
+  });
+  it('Teste se a função retorna o objeto correto para o parâmetro Melissandre', () => {
+    expect({
+      name: 'Melissandre',
+      class: 'Necromancer',
+      phrases: [ 'Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.' ]
+    }).toEqual(getCharacter('Melissandre'));
+  });
+  it('Teste se ao passar um nome que não está na tabela, a função retorna undefined.', () => {
+    expect(getCharacter('Fire')).toEqual(undefined);
+  });
+  it('Teste se o parâmetro não é Case Sensitive, ou seja, independente de conter letras maiúsculas ou minúsculas retorna o mesmo objeto relativo a ele', () => {
+    expect({
+      name: 'Melissandre',
+      class: 'Necromancer',
+      phrases: [ 'Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.' ]
+    }).toEqual(getCharacter('MeliSsandrE'));
   });
 });
